92a2587aadbf44a17f77c6ed56462ffc401e583d
CanadaApollo6 <riel.stamand@gmail.com>
2020-08-19 18:08:45 -0400

    Updates

---
+++ precache-manifest.fd75e05a4d2a8bc2840f8fe875ec3cdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "895aced0a41b93218cd5ca7aca9d2d8c",
+    "url": "/face-recognition-brain/index.html"
+  },
+  {
+    "revision": "4d57648d73a9e0954ef7",
+    "url": "/face-recognition-brain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "859e3d1590ae85a33c6a",
+    "url": "/face-recognition-brain/static/css/main.d786f31a.chunk.css"
+  },
+  {
+    "revision": "4d57648d73a9e0954ef7",
+    "url": "/face-recognition-brain/static/js/2.f83d8d55.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/face-recognition-brain/static/js/2.f83d8d55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "859e3d1590ae85a33c6a",
+    "url": "/face-recognition-brain/static/js/main.f89ded05.chunk.js"
+  },
+  {
+    "revision": "29e5821f791c973dcb04",
+    "url": "/face-recognition-brain/static/js/runtime-main.e3020e8e.js"
+  }
+]);
